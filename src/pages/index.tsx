bfee05ea4641c86d0d5f21ae53dec37fde8a4062
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-03-21 11:15:40 -0300

    inicio de integraçao tabela>form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ import Formulario from "../components/Formulario"
 
 export default function Home() {
 
+  const [visivel, setVisivel] = useState< 'tabela' | 'form'>('tabela')
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+
   const clientes = [
     new Cliente('Ana', 34, '1'),
     new Cliente('Bia', 45, '2'),
@@ -15,18 +18,24 @@ export default function Home() {
   ]
 
   function clienteSelecionado(cliente: Cliente){
-
+    setCliente(cliente)
+    setVisivel('form')
   }
 
   function clienteExcluido(cliente: Cliente) {
 
   }
 
-  function salvarCliente(cliente: Cliente) {
-
+  function novoCliente() {
+    setCliente(Cliente.vazio())
+    setVisivel('form')
   }
 
-  const [visivel, setVisivel] = useState< 'tabela' | 'form'>('tabela')
+  function salvarCliente(cliente: Cliente) {
+    setVisivel('tabela')
+  }
+
+
 
   return (
     <div className={`
@@ -40,7 +49,7 @@ export default function Home() {
               <Botao 
                 cor="green" 
                 className="mb-4"
-                onClick={() => setVisivel('form')}
+                onClick={novoCliente}
                 >
                 Novo Cliente
               </Botao>
@@ -52,7 +61,7 @@ export default function Home() {
           </>
         ): 
           <Formulario 
-            cliente={clientes[1]}
+            cliente={cliente}
             clienteMudou={salvarCliente}
             cancelado={() => setVisivel('tabela')}
 
